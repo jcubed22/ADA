e98ba6bd05a64c9e16f60d7cab0d73bb41cc34d8
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-09-30 12:05:48 -0700

    Add initial files for Angular use

---
+++ app.js
@@ -0,0 +1,8 @@
+var ada = angular.module('ada', ['ui.router']);
+
+ada.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+});
