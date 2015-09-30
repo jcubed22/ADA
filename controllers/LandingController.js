55b8a3908f327f042d5ed9f620c44b9c7d37dea8
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-09-30 14:16:03 -0700

    Update countdown methods to display in Angular

--- controllers/LandingController.js
+++ controllers/LandingController.js
@@ -1,16 +1,19 @@
-ada.controller('LandingCtrl', function LandingCtrl($scope) {
-  var counter = 10;
-  var countdown = setInterval(function() {
-    console.log(counter);
-    counter--
-    if (counter === 0) {
-      console.log("explosion!");
-      clearInterval(countdown);
-    }
-  }, 1000);
+ada.controller('LandingCtrl', function LandingCtrl($scope, $interval) {
+
+  $scope.counter = 10;
+
+  $scope.startCountdown = function() {
+    $scope.countdown = $interval(function() {
+      $scope.counter--
+      if ($scope.counter === 0) {
+        $scope.counter = "explosion!";
+        $interval.cancel($scope.countdown);
+      }
+    }, 1000);
+  }
 
   $scope.stopCountdown = function() {
-    clearInterval(countdown);
-    console.log(counter);
+    $interval.cancel($scope.countdown);
+    alert("You saved us!");
   }
 });
