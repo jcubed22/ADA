f0203761340f5cc1a5f75cc30a1e5468df9b5553
Jordan Johansen <jordanjohan@gmail.com>
2015-10-07 16:10:31 -0700

    Add parts and total parts found to Inventory List.

--- services/InventoryFactory.js
+++ services/InventoryFactory.js
@@ -1,10 +1,11 @@
 ada.factory('InventoryFactory', function InventoryFactory() {
   var factory = {};
 
+
   //Ship Parts
   factory.kaniflin_pin = 0;
   factory.whangdoodle = 0;
-  factory.proton_distributor = 0;
+  factory.proton_redistributor = 0;
 
   //Find parts
   factory.find_pin = function () {
@@ -16,7 +17,12 @@ ada.factory('InventoryFactory', function InventoryFactory() {
   };
 
   factory.find_distributor = function () {
-    factory.proton_distributor = 1;
+    factory.proton_redistributor = 1;
+  };
+
+  factory.find_part = function () {
+    factory.parts_found = factory.kaniflin_pin + factory.whangdoodle + factory.proton_redistributor;
+    return factory.parts_found;
   };
 
   return factory;
