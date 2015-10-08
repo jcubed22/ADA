49a4cb80915438df35fe7dc722106b23267199a5
calascionec <chris.calascione@gmail.com>
2015-10-08 13:57:09 -0700

    finish first game

--- controllers/HomeBaseController.js
+++ controllers/HomeBaseController.js
@@ -23,13 +23,17 @@ ada.controller('HomeBaseCtrl', function HomeBaseCtrl($scope, $interval, Inventor
   $scope.refillOxygen = function () {
     InventoryFactory.count = 0;
     InventoryFactory.oxygenLeft = 400;
-    InventoryFactory.invertStyle = '';
+    InventoryFactory.myStyle = {'height': 400 +'px'};
   }
 
   $scope.test = function () {
     if (!InventoryFactory.wentOutside) {
       $scope.adaSpeak('Just kidding! We did crash though. Welcome to U\'tah!', 0);
-    } else {
+    } else if (InventoryFactory.oxygenLeft > 0 && (InventoryFactory.parts_found != 3)) {
+      $scope.adaSpeak("Get back out there! You can't leave without finding all three missing parts!", 0)
+    } else if (InventoryFactory.parts_found === 3) {
+      $scope.adaSpeak('You found all of the parts! On to the next challenge!', 0)
+    } else if ((InventoryFactory.parts_found != 3) && (InventoryFactory.oxygenLeft < 399.75)) {
       $scope.adaSpeak('You ran out of oxygen! Go back outside and try again!', 0);
     }
   }();
