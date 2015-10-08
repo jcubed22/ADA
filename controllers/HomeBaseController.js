8af27d5e08b2e50f70ff32afb8e54ee32d324ee9
Jordan Johansen <jordanjohan@gmail.com>
2015-10-08 12:10:00 -0700

    Pass InventoryFunction into HomeBaseCtrl scope.

--- controllers/HomeBaseController.js
+++ controllers/HomeBaseController.js
@@ -1,5 +1,6 @@
 ada.controller('HomeBaseCtrl', function HomeBaseCtrl($scope, $interval, InventoryFactory) {
 
+  $scope.InventoryFactory = InventoryFactory;
   console.log(InventoryFactory.wentOutside);
   $scope.message = "";
 
